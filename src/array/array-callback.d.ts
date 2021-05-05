e4029e71865271ea56affa0c90a4c5ed688f1c8c
drizzer14 <drizzer14@gmail.com>
2021-05-05 12:09:33 +0300

    feat(array): auto-curry array callback types

--- src/array/array-callback.d.ts
+++ src/array/array-callback.d.ts
@@ -1,27 +1,33 @@
-import type { Curry } from '../function'
+import type { Curry } from '../function/curry'
 
-export type ArrayCallback<T, R = T> = (
-  element: T,
-  index: number,
-  array: T[],
-) => R
+export type ArrayCallback<T, R = T> = Curry<
+  (
+    element: T,
+    index: number,
+    array: T[],
+  ) => R
+>
 
 export type Predicate<T> = ArrayCallback<T, boolean>
 
 export type Transformer<T, R = T> = ArrayCallback<T, R>
 
-export type LeftReducer<T, R = T> = (
-  accumulator: R,
-  current: T,
-  index: number,
-  array: T[],
-) => R
+export type LeftReducer<T, R = T> = Curry<
+  (
+    accumulator: R,
+    current: T,
+    index: number,
+    array: T[],
+  ) => R
+>
 
-export type RightReducer<T, R = T> = (
-  current: T,
-  accumulator: R,
-  index: number,
-  array: T[],
-) => R
+export type RightReducer<T, R = T> = Curry<
+  (
+    current: T,
+    accumulator: R,
+    index: number,
+    array: T[],
+  ) => R
+>
 
 export type Comparator<T> = Curry<(a: T, b: T) => number>
