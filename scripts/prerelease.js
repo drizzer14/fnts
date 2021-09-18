787402ca94dcf259d9b60b6be3e95ab8e83fd6ad
drizzer14 <drizzer14@gmail.com>
2021-09-18 23:10:53 +0300

    chore(scripts): replace tabs with spaces in `prerelease.js`

--- scripts/prerelease.js
+++ scripts/prerelease.js
@@ -3,55 +3,55 @@ const copyfiles = require('copyfiles')
 const { writeFileSync } = require('fs')
 
 const copyIntoLib = (fileName, onBeforeCopy = (f) => f) => {
-	writeFileSync(
-		resolve(
-			__dirname,
-			'../lib',
-			`${fileName}`,
-		),
-		JSON.stringify(
-			onBeforeCopy(
-				require(`../${fileName}`),
-			),
-			null,
-			2,
-		),
-	)
+  writeFileSync(
+    resolve(
+      __dirname,
+      '../lib',
+      `${fileName}`,
+    ),
+    JSON.stringify(
+      onBeforeCopy(
+        require(`../${fileName}`),
+      ),
+      null,
+      2,
+    ),
+  )
 }
 
 copyfiles(
-	[
-		'README.md',
-		'LICENSE',
-		'lib',
-	],
-	{},
-	() => {},
+  [
+    'README.md',
+    'LICENSE',
+    'lib',
+  ],
+  {},
+  () => {},
 );
 
 [
-	[
-		'tsconfig.json',
-		({
-			 compilerOptions: {
-				 declaration: _d,
-				 declarationDir: _dd,
-				 emitDeclarationOnly: _edo,
-				 rootDir: _rd,
-				 stripInternal: _si,
-				 ...compilerOptions
-			 },
-		 }) => ({ compilerOptions }),
-	],
-	[
-		'package.json',
-		({
-			 scripts: _s,
-			 devDependencies: _dd,
-			 ...packageJSON
-		 }) => packageJSON,
-	],
+  [
+    'tsconfig.json',
+    ({
+       compilerOptions: {
+         declaration: _d,
+         declarationDir: _dd,
+         emitDeclarationOnly: _edo,
+         rootDir: _rd,
+         stripInternal: _si,
+         ...compilerOptions
+       },
+     }) => ({ compilerOptions }),
+  ],
+  [
+    'package.json',
+    ({
+       scripts: _s,
+       devDependencies: _dd,
+       ...packageJSON
+     }) => packageJSON,
+  ],
 ].forEach(
-	([fileName, onBeforeCopy]) => copyIntoLib(fileName, onBeforeCopy),
+  ([fileName, onBeforeCopy]) => copyIntoLib(fileName, onBeforeCopy),
 )
 
